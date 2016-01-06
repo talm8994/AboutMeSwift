4d3fa476098984ab5129f5df8a703bbf33af32d0
talm8994 <tom.tom.pop13@gmail.com>
2016-01-06 12:22:25 -0700

    blah

--- AboutMeSwift/runningView.swift
+++ AboutMeSwift/runningView.swift
@@ -11,5 +11,14 @@ import UIKit
 
 class runningView : UIViewController
 {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
 }
